3a2d52f630c54db2efa8323aca3c064e210de849
amaresh435 <79443303+amaresh435@users.noreply.github.com>
2022-12-11 02:44:03 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,15 +1,8 @@
-terraform {
-  required_version = "~> 1.3.5"
-  required_providers {
-    google = "~> 4.45.0"
-  }
-}
-
 provider "google" {
 #  access_key = "GOOGYQRHQD6UMEYNAW4KRUF4"
 #  secret_key = "QSR4ZA8WcGpv12n4DRNra6utVOVNPq6Ss/8SJPl0"
   project = "internal-interview-candidates"
-  region  = "us-central1"
+
 }
 
 # Create a VPC network
