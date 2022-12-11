744a27763430e7027037f63ae8465390a46e381f
amaresh435 <79443303+amaresh435@users.noreply.github.com>
2022-12-11 23:51:11 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -18,14 +18,14 @@ module "vpc" {
     {
       subnet_name           = "public"
       subnet_ip             = "10.0.0.0/25"
-      subnet_region         = "us-west2"
+      subnet_region         = "us-central1"
       subnet_private_access = "false"
       subnet_flow_logs      = "false"
     },
     {
       subnet_name           = "private"
       subnet_ip             = "10.0.1.0/25"
-      subnet_region         = "us-west2"
+      subnet_region         = "us-central1"
       subnet_private_access = "true"
       subnet_flow_logs      = "false"
     }
